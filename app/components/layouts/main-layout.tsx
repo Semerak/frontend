862a19a25e8a8601d286a6a77167844cef583ff6
DanSemerak <dan.semerak@gmail.com>
2025-05-26 13:08:32 +0200

    removed unnecessary comments

--- app/components/layouts/main-layout.tsx
+++ app/components/layouts/main-layout.tsx
@@ -3,19 +3,15 @@ import { LanguagePicker } from '../ui/language-picker';
 export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col relative overflow-hidden">
-      {/* Absolute Language Picker */}
       <div className="absolute top-4 left-4 z-10 w-18">
         <LanguagePicker />
       </div>
 
-      {/* Page content with sticky footer layout */}
       <div className="flex flex-col flex-1">
-        {/* Main content area grows to fill remaining space */}
         <div className="flex-1 overflow-auto flex flex-col justify-start">
           {children}
         </div>
 
-        {/* Footer stuck to the bottom */}
         <footer className="flex justify-center pb-8  z-10">
           <img
             src="/beutechful-logo.png"
