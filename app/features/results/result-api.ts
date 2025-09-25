0b33a363d9a0eaec19139c02b1bcdfc187b04f9f
beka <beka@netlight.com>
2025-09-25 11:10:15 +0200

    cache results in local storage to reduce number of refetches

--- app/features/results/result-api.ts
+++ app/features/results/result-api.ts
@@ -1,5 +1,7 @@
 import type { Result } from '~/features/results/types';
 
+const LOCAL_STORAGE_KEY_PREFIX = 'results_';
+
 export async function postResults(payload: any): Promise<Result> {
   const url = `${import.meta.env.VITE_BACKEND_CLOUD_URL}/get_results`;
   const response = await fetch(url, {
@@ -15,7 +17,14 @@ export async function postResults(payload: any): Promise<Result> {
   return response.json();
 }
 
-export async function getResultsByUserId(userId: string): Promise<Result> {
+export async function getResultsByUserId(userId?: string): Promise<Result> {
+  const storageKey = LOCAL_STORAGE_KEY_PREFIX + userId;
+  const cached = localStorage.getItem(storageKey);
+
+  if (cached) {
+    return JSON.parse(cached);
+  }
+
   const url = `${import.meta.env.VITE_BACKEND_CLOUD_URL}/get_results_by_user_id/${userId}`;
   const response = await fetch(url, {
     method: 'GET',
@@ -26,5 +35,13 @@ export async function getResultsByUserId(userId: string): Promise<Result> {
   if (!response.ok) {
     throw new Error('Failed to get results');
   }
-  return await response.json();
+  const data = await response.json();
+
+  try {
+    localStorage.setItem(storageKey, JSON.stringify(data));
+  } catch {
+    console.error('Failed cache results on localStorage');
+  }
+
+  return data;
 }
